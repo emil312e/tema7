3dc79e100b66ac96f609d74d83650333bb564ddb
Emily Orry Bendtzen <emilybendtzen@MacBook-Pro-tilhrende-Emily.local>
2021-02-01 11:57:57 +0100

    opgave 01

---
+++ script.js
@@ -0,0 +1,8 @@
+      document.querySelector("article").addEventListener("click", doSomeThing);
+
+      function doSomeThing() {
+          this.style.background = "red";
+      }
+
+      let greeting = "Hello World";
+      console.log(greeting);
